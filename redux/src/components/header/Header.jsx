8b8b0d23512c64db346348f3b6ecaa6ba1459f8a
Roman Trynko <trynkoroman25@gmail.com>
2022-06-25 19:59:09 +0300

    header

--- redux/src/components/header/Header.jsx
+++ redux/src/components/header/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
 import Logo from '../../assets/react.png';
+import { links } from '../../constants';
+
 import './Header.scss';
 
 export function Header(props) {
   return (
-    <div className="my-header">
+    <header className="my-header navbar">
       <img className="my-header-logo" src={Logo} alt="Logo" />
-    </div>
+
+      <div className="my-header-links-wrapper">
+        {links.map((link, key) => {
+          return (
+            <div className="nav-item" key={key}>
+              <a className="my-header-links-wrapper-link" href={link.url}>
+                {link.name}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </header>
   );
 }
